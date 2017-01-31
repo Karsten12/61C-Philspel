1c112aa916682c5f674064bdc790629b7db7b5be
Karsten Fonseca <kfonseca@Karstens-MacBook-Pro.local>
2017-01-31 11:56:50 -0800

    First commit w/ starter code

---
+++ cmake-build-debug/CMakeFiles/proj1_ami.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj1_ami.dir/hashtable.c.o"
+  "CMakeFiles/proj1_ami.dir/philspel.c.o"
+  "proj1_ami.pdb"
+  "proj1_ami"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/proj1_ami.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
